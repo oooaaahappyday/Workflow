18142061f3d32071d24f524425367f1be7372bec
oooaaahappyday <oooaaahappyday@users.noreply.github.com>
2017-05-22 15:38:28 +0200

    bugfix du js désormais fonctionnel

--- js/app.js
+++ js/app.js
@@ -1,18 +1,11 @@
+function commentAge () {
+	var age = parseInt(document.getElementById("age").value);
 
-
-function commentAge (age) {
-	var age = document.getElementById("age").textContent;
-	console.log(age);
-
-	switch (age) {
-		case age <= 10 :
-			alert ("Vous êtes trop jeune pour jouer au jeu Titre.");
-			break;
-		case (age > 10) && (age<= 70) :
-			alert ("Allez-y, mais allez-y mollo, il faut rester dans le political correct");
-			break;
-		case age > 70 :
-			alert ("Vous n'avez plus rien à perdre, le plus graveleux sera le mieux");
-			break;
-	};
+	if (age <= 10) {
+		console.log("Vous êtes trop jeune pour jouer au jeu Titre.");
+	} else if (age > 10 && age <= 70) {
+		console.log("Allez-y, mais allez-y mollo, il faut rester dans le political correct");
+	} else if (age > 70 ){
+		console.log("Vous n'avez plus rien à perdre, le plus graveleux sera le mieux");
+	}
 }
