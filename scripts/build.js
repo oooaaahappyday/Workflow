29702d36008ffe445fed5ba7e6359a1c4db35513
oooaaahappyday <oooaaahappyday@users.noreply.github.com>
2017-05-26 15:50:41 +0200

    ajout de gulp et du main-built.js qui est un fichier compilant tous les fichiers js en version minifiÃ©

---
+++ scripts/build.js
@@ -0,0 +1,12 @@
+({
+	baseUrl: ".",
+	paths: {
+		main: "lib/main",
+		jquery: "lib/jquery",
+		getAge: "modules/getAge",
+		ageComment: "modules/ageComment",
+		module: "modules/module.js"
+	},
+	name: "main",
+	out: "main-built.js"
+})
